9f75d23400060e4bcd8c124c920af5880925ab44
Kshitiz <84060730+katsusucks@users.noreply.github.com>
2024-04-15 08:15:12 +0545

    Update index.js
    Fixed bugs

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const axios = require('axios');
 
-const runWebhook = async (webhookUrl, apiEndpoint, desiredData, intervalTime) => {
-  setInterval(async () => {
-    try {
-      const response = await axios.get(apiEndpoint);
-      let data = response.data;
-      for (const key of desiredData) {
-        data = data[key];
-      }
-      const payload = {
-        content: data
-      };
-      await axios.post(webhookUrl, payload);
-    } catch (error) {
-      console.error(error);
+async function sendImages(webhookUrl, apiUrl, numberOfImages, method = 'GET') {
+  try {
+    let response;
+    if (method.toUpperCase() === 'GET') {
+      response = await axios.get(`${apiUrl}&limit=${numberOfImages}`);
+    } else if (method.toUpperCase() === 'POST') {
+      response = await axios.post(apiUrl, { numberOfImages });
+    } else {
+      throw new Error('Unsupported HTTP method. Only GET and POST are supported.');
     }
-  }, intervalTime * 1000);
-};
 
-module.exports = runWebhook;
+    if (response.data && response.data.images && response.data.images.length > 0) {
+      for (let i = 0; i < numberOfImages; i++) {
+        const url = response.data.images[i].url;
+        await axios.post(webhookUrl, { content: url });
+        console.log(`Image ${i + 1} sent to webhook successfully.`);
+      }
+    } else {
+      throw new Error('No images found in the API response.');
+    }
+  } catch (error) {
+    console.error('Error sending images to webhook:', error.message);
+  }
+}
+
+module.exports = {
+  sendImages
+};
